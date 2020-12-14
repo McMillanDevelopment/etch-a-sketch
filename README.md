88e5f3f1d12a3daea16b8bb04386bc6df74c814a
Michael McMillan <mcmillanmikea@gmail.com>
2020-12-14 13:33:06 -0800

    Added button to reset grid and input a new grid size

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Etch-a-sketch project from The Odin Project.
 https://www.theodinproject.com/courses/foundations/lessons/etch-a-sketch-project
 
+Initial Functionality in place.
+
+Added button to reset grid and input a new size.
--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-// Create a 16x16 grid of square divs
-
+// // variables
+const button = document.querySelector('button');
 const container = document.querySelector('#container');
-const gridDiv = document.getElementsByClassName('.grid');
-let arr = [];
-let div = ""
+let newGridSize = 0;
 
 
-for(i = 0; i <= 255; i++) {
-    div = document.createElement('div');
-    div.classList.add('grid');
-    div.textContent = i + 1;
-    div.addEventListener('mouseover', function(e){
-        e.target.classList.add('mouse');
-    });
-    arr.push(div);
-    container.appendChild(div);
+// Button
+button.addEventListener('click', function(){
+    const div = document.querySelectorAll('.grid');
+    for(i = 0; i < div.length; i ++) {
+        div[i].remove(div);
+    };
+    createGrid(newGridArea());
+});
+
+// // CSS grid to create the grid area
+function createGrid(gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+    for(i = 1; i <= gridArea; i++) {
+        // console.log(gridArea);
+        let gridItem = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
+        // gridItem.innerText = i;
+        gridItem.classList.add('grid')
+        gridItem.addEventListener('mouseover', function(e){
+            e.target.classList.add('mouse');
+        });
+        container.appendChild(gridItem);
+    };
 };
 
+function newGridArea () {
+    newGridSize = parseInt(prompt('What size for the next grid?', '16'));
+    if(newGridSize >= 17 ) {
+        newGridSize = parseInt(prompt('Max grid size is 16', '16'));
+    };
+    return newGridSize;
+};
+
+function colorChange() {
+
+};
+
+// function calls
+createGrid(5);
